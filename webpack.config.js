7e8b8794995ace99e5bfc6268997978ffb62d09b
Glitch (pw20) <none>
2019-08-30 18:18:33 +0000

    First Commit, blank webpack starter

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  context: path.join(__dirname, './'),
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'app'),
+      },
+    ],
+  },
+  node: {
+    fs: 'empty'
+  }
+};
