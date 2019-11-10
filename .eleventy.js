78af3f6f65dae30959b91d6f15aaae47d0e5ceef
Glitch (pw20) <none>
2019-11-10 16:26:27 +0000

    🚟💨 Checkpoint ./build/blog/index.hbs:217543/125 ./build/portfolio/index.hbs:217543/1144 ./build/_includes/portfolioIndex.hbs:217543/804 ./build/_includes/blogIndex.hbs:217543/70 ./watch.json:217543/315 ./.eleventy.js:217543/177

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,14 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.setDataDeepMerge(true);
+  
+  eleventyConfig.addCollection("postsReversed", function(collection) {
+    // get unsorted items
+    return collection.getAll();
+  });
+
+  
+  
   return {
     dir: {
       input: "build",
