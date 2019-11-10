08ff63ce436551695265778a27286e5e1df8ba54
Glitch (pw20) <none>
2019-11-10 16:38:27 +0000

    🍍📻 Checkpoint ./.eleventy.js:217543/648 ./build/_includes/blogIndex.hbs:217543/18

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,21 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.setDataDeepMerge(true);
   
+  // Create postsReversed tag with posts tag in reverse order
   eleventyConfig.addCollection("postsReversed", function(collection) {
-    // get unsorted items
-    return collection.getAll();
+   return collection.getFilteredByTag("posts").reverse();
+  });
+  
+  eleventyConfig.addCollection("portfolioSorted", function(collection) {
+   
+    var filter = collection.getFilteredByTag("projects");
+    for (var i in filter) {
+      var j = filter[i];
+      console.log(i, j);
+    }
+    
+    
+    return collection.getFilteredByTag("projects");
   });
 
   
