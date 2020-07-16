d002755ab932e8a0c774ddf4e3b51823a27f4661
Patrick Weaver <pjpwea@gmail.com>
2020-07-16 12:12:34 -0400

    refactor and restyle preview text

--- helpers/general.js
+++ helpers/general.js
@@ -23,9 +23,19 @@ module.exports = {
       }
       
       let previewText = htmlToText.fromString(marked(body), options).substring(0, 349);
-      let lastChar = previewText.substring(348, 349);
+      // Make this more general
+      weirdCharIndex = previewText.indexOf(">");
+      if (weirdCharIndex >=0) {
+        previewText = previewText.substring(0, weirdCharIndex);
+      }
+
+      let lastChar = previewText.substring(previewText.length - 1, previewText.length);
+      while (lastChar === " " || lastChar === "\n") {
+        lastChar = previewText.substring(previewText.length - 1, previewText.length);
+        previewText = previewText.substring(0, previewText.length - 1)
+      }
       
-      if (lastChar === '.' || lastChar === ',') {
+      if (lastChar === '.' || lastChar === ',' || lastChar === "!") {
         previewText = previewText.substring(0, 348);
       }
       
