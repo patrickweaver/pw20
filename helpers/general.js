76e144009527e11a437791212efb01179d25265f
Patrick Weaver <pjpwea@gmail.com>
2022-02-19 15:02:02 -0500

    Upgrade dependencies

--- helpers/general.js
+++ helpers/general.js
@@ -1,18 +1,17 @@
 const htmlToText = require("html-to-text");
-const marked = require("marked");
+const { marked } = require("marked");
 
 module.exports = {
-
-  isSameString: function(stringA, stringB) {
+  isSameString: function (stringA, stringB) {
     return stringA.toUpperCase() === stringB.toUpperCase();
   },
-  
-  eq: function() {
+
+  eq: function () {
     const args = Array.prototype.slice.call(arguments, 0, -1);
-    return args.every(expression => args[0] === expression);
+    return args.every((expression) => args[0] === expression);
   },
 
-  or: function() {
+  or: function () {
     const args = Array.prototype.slice.call(arguments, 0, -1);
     for (let i in args) {
       if (args[i]) return true;
@@ -20,39 +19,45 @@ module.exports = {
     return false;
   },
 
-  not: a => !a,
-  
-  previewText: function(body) {
-    if (body){
+  not: (a) => !a,
+
+  previewText: function (body) {
+    if (body) {
       const options = {
         ignoreHref: true,
         ignoreImage: true,
-        uppercaseHeadings: false
-      }
-      const plainText = htmlToText.fromString(marked(body), options)
+        uppercaseHeadings: false,
+      };
+      const plainText = htmlToText.fromString(marked.parse(body), options);
       let previewText = plainText.substring(0, 349);
       // Make this more general
       weirdCharIndex = previewText.indexOf(">");
-      if (weirdCharIndex >=0) {
+      if (weirdCharIndex >= 0) {
         previewText = previewText.substring(0, weirdCharIndex);
       }
 
-      let lastChar = previewText.substring(previewText.length - 1, previewText.length);
+      let lastChar = previewText.substring(
+        previewText.length - 1,
+        previewText.length
+      );
       while (lastChar === " " || lastChar === "\n") {
-        lastChar = previewText.substring(previewText.length - 1, previewText.length);
-        previewText = previewText.substring(0, previewText.length - 1)
+        lastChar = previewText.substring(
+          previewText.length - 1,
+          previewText.length
+        );
+        previewText = previewText.substring(0, previewText.length - 1);
       }
-      
-      if (lastChar === '.' || lastChar === ',' || lastChar === "!") {
+
+      if (lastChar === "." || lastChar === "," || lastChar === "!") {
         previewText = previewText.substring(0, 348);
       }
 
       if (plainText.length > previewText.length) {
-        previewText += '…';
+        previewText += "…";
       }
-      
+
       return previewText;
     }
-    return '';
-  }
-}
+    return "";
+  },
+};
